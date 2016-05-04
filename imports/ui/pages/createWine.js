c31095fe8bbc431f3d5d9b15ff93a71ac9954b1c
Mike Lerch <mike.in.flames@gmail.com>
2016-05-04 09:17:53 +0200

    Create Wine now lets user input notes.

--- imports/ui/pages/createWine.js
+++ imports/ui/pages/createWine.js
@@ -13,6 +13,7 @@ Template.createWine.events({
         event.preventDefault();
 
         const name = template.find("#id_winename").value;
+        const notes = template.find("#id_winenotes").value;
 
         if (name == "") {
             return;
@@ -20,12 +21,14 @@ Template.createWine.events({
 
         const wine = new Wine({
             name: name,
+            notes: notes,
             createdAt: new Date()
         });
         wine.save();
 
         // Clear form
         template.find("#id_winename").value = "";
+        template.find("#id_winenotes").value = "";
         template.find("#id_winename").focus();
     }
 });
